0b6a9bc5d52ccb1a9a001e2500aaf8e5e8a54332
Akzhol Kabidulla <kabidullaakzol@gmail.com>
2026-02-06 03:38:49 +0500

    Add_Practice2_python_control_flow_examples_if_else_statement

---
+++ if_else/if_statement.py
@@ -0,0 +1,19 @@
+
+
+if __name__ == "__main__":
+    temperature = 28
+
+    if temperature > 25:
+        print("Жарко")
+
+    score = 90
+    if score >= 90:
+        print("Отлично!")
+
+    name = "Жасаби"
+    if name:
+        print("Имя введено:", name)
+
+    n = 11
+    if n % 2 == 0:
+        print(n, "чётное")
