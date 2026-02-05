eaa3577573ef6247c3800923cba658ddc0ef739f
sssbck <zhasabi05@gmail.com>
2026-02-05 23:06:08 +0500

    Add_Practice2_python_control_flow_examples_boolean_comparison

---
+++ boolean/boolean_comparison.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    a = 10
+    b = 7
+
+    print("a == b:", a == b)
+    print("a != b:", a != b)
+    print("a > b:", a > b)
+    print("a < b:", a < b)
+    print("a >= 10:", a >= 10)
+    print("b <= 7:", b <= 7)
+
+    print("'apple' < 'banana':", "apple" < "banana")
+
+    s1 = "python"
+    s2 = "plc"
+    print("len(s1) > len(s2):", len(s1) > len(s2))
+
+    x = [1, 2, 3]
+    y = [1, 2, 3]
+    print("x == y:", x == y)  
+    print("x is y:", x is y) 
